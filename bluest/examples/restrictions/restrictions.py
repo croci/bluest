10a5b47b4792f7c880a31bec58d3e5e0a6ea3c47
croci <matteo.croci@maths.ox.ac.uk>
2022-01-24 13:12:18 -0600

    Updated restrictions script. Estimating E[Pf-Pc] and V[Pf-Pc] seems to be the solution. This now works.

--- bluest/examples/restrictions/restrictions.py
+++ bluest/examples/restrictions/restrictions.py
@@ -18,31 +18,38 @@ RNG = RandomState(mpiRank)
 dim = 2 # spatial dimension
 buf = 1
 n_levels  = 7
+No = 2
 
 meshes = [RectangleMesh(MPI.comm_self, Point(0,0), Point(1,1), 2**l, 2**l) for l in range(buf, n_levels+buf)][::-1]
 
 function_spaces = [FunctionSpace(mesh, 'CG', 1) for mesh in meshes]
 
-left   = CompiledSubDomain("near(x[0], 0) && on_boundary")
-right  = CompiledSubDomain("near(x[0], 1) && on_boundary")
-bottom = CompiledSubDomain("near(x[1], 0) && on_boundary")
-top    = CompiledSubDomain("near(x[1], 1) && on_boundary")
-
 def get_bcs(V, sample):
     _,b,c,d = sample # a = 0
 
-    b = math.exp(b); c = c**2; d = math.sqrt(math.fabs(d))
-    bottom_bexpr = Expression("b*sin(4*DOLFIN_PI*x[0])", b=b, degree=3, mpi_comm=MPI.comm_self)
-    left_bexpr   = Expression("c*sin(2*DOLFIN_PI*x[1])",  c=c, degree=3, mpi_comm=MPI.comm_self)
-    top_bexpr    = Expression("c + (d-c)*x[0]", c=c, d=d, degree=1, mpi_comm=MPI.comm_self)
-    right_bexpr  = Expression("b + (d-b)*x[1]", b=b, d=d, degree=1, mpi_comm=MPI.comm_self)
+    bc_expr = Expression('2.0*b*x[0]*x[1] + c*x[0]*x[0] + d*x[1]*x[1] + 1.0', b=b, c=c, d=d, degree=2, mpi_comm=MPI.comm_self)
 
-    left_bc   = DirichletBC(V, left_bexpr, left)
-    right_bc  = DirichletBC(V, right_bexpr, right)
-    top_bc    = DirichletBC(V, top_bexpr, top)
-    bottom_bc = DirichletBC(V, bottom_bexpr, bottom)
+    bc = DirichletBC(V, bc_expr, "on_boundary")
 
-    return [left_bc, right_bc, top_bc, bottom_bc]
+    return [bc]
+
+def compute_solution(V, sample):
+    u = TrialFunction(V)
+    v = TestFunction(V)
+
+    sol = Function(V)
+
+    D = Constant(exp(sample[0]))
+    f = Expression('exp(-(pow(x[0]-0.5,2)+pow(x[1]-0.5,2)))', degree=4, mpi_comm=MPI.comm_self)
+
+    lhs = inner(D*grad(u), grad(v))*dx + u*v*dx
+    rhs = f*v*dx
+
+    bcs = get_bcs(V, sample)
+
+    solve(lhs == rhs, sol, bcs)
+
+    return sol
 
 class PoissonProblem(BLUEProblem):
     def sampler(self, ls, N=1):
@@ -53,36 +60,30 @@ class PoissonProblem(BLUEProblem):
     def evaluate(self, ls, samples, N=1):
 
         L = len(ls)
-        out = [0 for i in range(L)]
+        out = [[0 for i in range(L)] for n in range(No)]
 
         for i in range(L):
-            if ls[i] > n_levels-1:
-                out[i] = sum(samples[i]**2)
-                continue
 
-            V = function_spaces[ls[i]]
+            l = ls[i]
+            V = function_spaces[l]
 
-            u = TrialFunction(V)
-            v = TestFunction(V)
+            sol = compute_solution(V, samples[i])
 
-            sol = Function(V)
+            Pf = assemble(sol*sol*dx)
 
-            D = Constant(exp(samples[i][0]))
-            f = Constant(1.0) #Expression("e*sin(exp(x[0]*x[1])) + (1-e)*exp(3*cos(x[1]+x[0]))", degree=3, e=samples[i][-1]**2, mpi_comm=MPI.comm_self)
+            if l < M-2:
+                Vc = function_spaces[l+1]
+                solc = compute_solution(Vc, samples[i])
+                Pc = assemble(solc*solc*dx)
+            else:
+                Pc = 0.0
 
-            lhs = inner(D*grad(u), grad(v))*dx + u*v*dx
-            rhs = f*v*dx
+            out[0][i] = Pf
+            out[1][i] = Pf - Pc
 
-            bcs = get_bcs(V, samples[i])
+        return out
 
-            solve(lhs == rhs, sol, bcs)
-
-            out[i] = assemble(sol*sol*dx)
-            #out[i] = assemble(exp(sin(sol))*dx)
-
-        return [out]
-
-M = n_levels + 1
+M = n_levels
 
 def build_test_covariance(string="full"):
     C = np.nan*np.ones((M,M))
@@ -116,36 +117,32 @@ def build_test_covariance(string="full"):
 ndofs = np.array([V.dim() for V in function_spaces])
 costs = np.concatenate([ndofs, [1]])
 
-C = build_test_covariance("--O")
+C = [build_test_covariance("--O") for n in range(No)]
 load_model_graph = os.path.exists("./restrictions_model_data.npz")
 if load_model_graph:
-    problem = PoissonProblem(M, costs=costs, datafile="restrictions_model_data.npz")
+    problem = PoissonProblem(M, n_outputs=No, costs=costs, datafile="restrictions_model_data.npz")
 else:
-    problem = PoissonProblem(M, C=C, costs=costs, covariance_estimation_samples=10000)
+    problem = PoissonProblem(M, n_outputs=No, C=C, costs=costs, covariance_estimation_samples=1000)
     problem.save_graph_data("restrictions_model_data.npz")
 
 C = problem.get_covariance()
 true_C = C.copy()
 
-C0 = C[2:-1,2:-1].copy()
+d = np.diag(C)
+delv = np.diag(problem.get_covariance(1))
+covest_ex = (d[:-1] + d[1:] - delv[:-1])/2
 
-d = np.diag(C0)
-d1 = np.diag(C0,1)
-diffv = abs(d[:-1] - d[1:])
-vdiff = d[:-1] + d[1:] - 2*d1
+vals = np.array(problem.evaluate(list(range(M)), [np.zeros(4) for i in range(M)])[0])
+valdiff = abs(vals[:-1]-vals[1:])
+m = 2*np.polyfit(np.log2(ndofs[2:][:3]), np.log2(valdiff[2:][:3]), 1)[0]
 
-dvp = np.polyfit(np.log2(ndofs[3:][:3]), np.log2(diffv[:3]), 1)
-vdp = np.polyfit(np.log2(ndofs[3:][:3]), np.log2(vdiff[:3]), 1)
+r = (ndofs[2]/ndofs[3])**m
+v0 = (r*d[2]-d[3])/(r-1)
+v1 = v0
+delv_est = 2.**np.polyval(np.polyfit(np.log2(ndofs[3:][:3]), np.log2(delv[:3]), 1), np.log2(ndofs[1:3]))
+covest_est = np.array([v0+v1 - delv_est[0], v1 + d[2] - delv_est[1]])/2
 
-v1 = d[0] + 2.**(np.log2(ndofs[2])*dvp[0] + dvp[1])
-v1t = np.median(d[:3]) - 2.**(np.log2(ndofs[2])*dvp[0] + dvp[1])
-v0 = d[0] + 2.**(np.log2(ndofs[1])*dvp[0] + dvp[1])
-v0t = np.median(d[:3]) - 2.**(np.log2(ndofs[1])*dvp[0] + dvp[1])
-
-delv12 = 2.**(np.log2(ndofs[2])*vdp[0] + vdp[1])
-delv01 = 2.**(np.log2(ndofs[1])*vdp[0] + vdp[1])
-c12 = 0.5*(v1t + np.median(d[:3]) - delv12)
-c01 = 0.5*(v0t + v1t - delv01)
+c01,c12 = covest_est
 
 C[0,0] = v0
 C[1,1] = v1
@@ -159,67 +156,25 @@ C[1,2],C[2,1] = c12,c12
 
 C[np.isnan(C)] = np.inf
 
-eps = 1.e-3
+eps = 5.e-2
 
 problem.setup_solver(K=3, eps=eps)
+problem.setup_mlmc(eps=eps)
+problem.setup_mfmc(eps=eps)
 
-problem = PoissonProblem(M, C=C, costs=costs, skip_projection=True)
+problem = PoissonProblem(M, C=C, costs=costs)
 
 problem.setup_solver(K=3, eps=eps)
+problem.setup_mlmc(eps=eps)
+problem.setup_mfmc(eps=eps)
 
-sys.exit(0)
+C_hat = problem.get_covariance(); C_hat[np.isnan(C_hat)] = 0
+C[~np.isfinite(C)] = 0
+rel_err = np.linalg.norm(C-C_hat)/np.linalg.norm(C)
+err = np.sqrt((v0-true_C[0,0])**2 + (v1 - true_C[1,1])**2 + (c01 - true_C[0,1])**2 + (c12 - true_C[1,2])**2)
 
-####################################################################################
+if verbose: print("Covariance estimattion errors. Matrix rel err: ", rel_err, "Entries abs err: ", err)
 
-complexity_test = False
-standard_MC_test = False
-comparison_test = False
-variance_test = False
-
-if complexity_test:
-    eps = 2**np.arange(3,8)
-    tot_cost, rate = problem.complexity_test(eps, K=3)
-    sys.exit(0)
-
-if standard_MC_test:
-    eps = 0.1
-    problem.setup_solver(K=3, eps=eps, solver="gurobi")
-    out = problem.solve()
-    out_MC = problem.solve_mc(eps=eps)
-    if verbose: print("BLUE (mu, err, cost):", out)
-    if verbose: print("MC   (mu, err, cost):", out_MC)
-    sys.exit(0)
-
-if comparison_test:
-    budget = 10.;  eps    = None
-
-    out_MLMC = problem.setup_mlmc(budget=budget, eps=eps)
-    out_MFMC = problem.setup_mfmc(budget=budget, eps=eps)
-    out      = problem.setup_solver(K=M, budget=budget, eps=eps, solver="cvxpy")
-
-    if verbose: print("\nMLMC. Errors: %s. Total cost: %f." % (out_MLMC[1]["errors"], out_MLMC[1]["total_cost"]))
-    if verbose: print("MFMC. Errors: %s. Total cost: %f." % (out_MFMC[1]["errors"], out_MFMC[1]["total_cost"]))
-    if verbose: print("BLUE. Errors: %s. Total cost: %f.\n\n" % (out[1]["errors"],      out[1]["total_cost"]))
-
-    eps    = 0.25;  budget = None
-
-    out_MLMC = problem.setup_mlmc(budget=budget, eps=eps)
-    out_MFMC = problem.setup_mfmc(budget=budget, eps=eps)
-    out      = problem.setup_solver(K=M, budget=budget, eps=eps, solver="cvxpy")
-
-    if verbose: print("\nMLMC. Errors: %s. Total cost: %f." % (out_MLMC[1]["errors"], out_MLMC[1]["total_cost"]))
-    if verbose: print("MFMC. Errors: %s. Total cost: %f." % (out_MFMC[1]["errors"], out_MFMC[1]["total_cost"]))
-    if verbose: print("BLUE. Errors: %s. Total cost: %f." % (out[1]["errors"],      out[1]["total_cost"]))
-
-    sys.exit(0)
-
-if variance_test:
-    budget = 1.; eps = None
-    err_ex, err = problem.variance_test(budget=budget, eps=eps, K=3, N=100)
-    sys.exit(0)
-
-#problem.setup_solver(K=3, budget=10., solver="cvxpy")
-problem.setup_solver(K=3, eps=0.1, solver="cvxpy")
-
-out = problem.solve()
-if verbose: print(out)
+if perform_variance_test:
+    err_ex, err = problem.variance_test(eps=eps, K=3, N=100)
+    if verbose: print(err_ex, err)
